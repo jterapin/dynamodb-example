5f7ae2c6c529b1c19267659b2c9ec2541a453867
Juli Tera <terajul@amazon.com>
2022-09-02 15:01:07 -0400

    Created a ThreadRecord class and added an auto-update time method

--- aws_record_example.rb
+++ aws_record_example.rb
@@ -10,3 +10,39 @@ end
 
 # .find is a class method associated with Aws::Record
 #  need to pass in primary key
+
+# Thread class
+#
+class ThreadRecord
+  include Aws::Record
+  set_table_name "Thread"
+  string_attr :ForumName, hash_key: true
+  string_attr :rk, range_key: true, database_attribute_name: 'Subject'
+  epoch_time_attr :Updated
+
+  # overriding class method from Aws::Record
+  # need to look into the source code to match opts
+  def save(opts = {})
+    # passing in options from the save class from source code
+    self.Updated = Time.now
+    super opts
+  end
+
+end
+
+# .find to see if the class exists
+# .to_h to look inside of an objectt.
+# method that ends with ? means return true or false
+# method that ends with ! means dangerous, mutate original data
+
+# save! checks to see if the object has all info to perform save
+# if object method has raise errors, the method might be marked with !
+
+# dirty method on class will show what attributes has been changed on local machine
+
+# Debug class
+class Debug
+  include Aws::Record
+  string_attr :id, hash_key: true
+  string_attr :rk, range_key: true
+end
